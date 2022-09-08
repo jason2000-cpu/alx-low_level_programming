15ee98e2428a61bd7229604c2a1f34565384ca54
jackson <jasonmwangi2000@gmail.com>
2022-09-08 13:23:11 -0700

    modified

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+/**
+ * main - Entry pint
+ * 
+ * Description: 'Print  a string with printf()'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, tu the outcome is a piece of art,"\n);
+	return (0)
+}
