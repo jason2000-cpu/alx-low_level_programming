7ea983e38568e868f5d9816f053c69853a05cf3e
jackson <jasonmwangi2000@gmail.com>
2022-09-08 08:04:06 -0700

    print string using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 /*The main function prints the result of the puts*/
 int main(void)
 {
+	/*to print the string using puts()*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
