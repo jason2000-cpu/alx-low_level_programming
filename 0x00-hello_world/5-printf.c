c365ec814e72ecc97a8ff88594a3b49b5579317f
jackson <jasonmwangi2000@gmail.com>
2022-09-08 23:51:28 -0700

    print string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	printf("with proper grammer, tu the outcome is a piece of art,"\n);
-	return (0)
+	printf("with proper grammer, tu the outcome is a piece of art,\n");
+	return (0);
 }
