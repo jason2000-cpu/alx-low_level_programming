34a582da095d50d07bf81d3f010a345fbd01ea5f
jackson <jasonmwangi2000@gmail.com>
2022-09-09 02:02:28 -0700

    print string using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 * Description: 'Print a string using write()'
@@ -7,6 +8,6 @@
 **/
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
