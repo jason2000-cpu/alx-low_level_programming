8e46b3d68f342fc422c55e992d6cfe953dfb470d
jackson <jasonmwangi2000@gmail.com>
2022-09-09 08:20:53 -0700

    print using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry Point
+ *
+ * Description: This func prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
