0354dbf75a66ffad9d317e601e83bdf4fdf29d31
jackson <jasonmwangi2000@gmail.com>
2022-09-09 07:21:51 -0700

    print alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
 * main- Entry point
-* Description: 'This func print alphabets in lowercase'
+* Description: This func print alphabets in lowercase
 * Return: Always 0 (Success)
 */
 int main(void)
