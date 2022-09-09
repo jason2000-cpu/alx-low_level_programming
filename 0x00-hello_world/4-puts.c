ccccff696ce88c48c3d6f6e2a7c0e24dd64e8162
jackson <jasonmwangi2000@gmail.com>
2022-09-09 00:22:37 -0700

    prints string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Entry point
- * Description - 'Prints put a string using puts'
- *
- * Return - Always 0 (Success)
- */
+* main -Entry point
+* Description: 'Prints put a string using puts'
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
