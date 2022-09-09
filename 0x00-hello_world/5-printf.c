c29bf444192919800d4436e933b8dbf489aa4886
jackson <jasonmwangi2000@gmail.com>
2022-09-09 00:01:43 -0700

    print string using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry pint
- * 
- * Description:'Print  a string with printf()'
- *
- * Return: Always 0 (Success)
- */
-
+* main - Entry pint
+* 
+* Description:'Print  a string with printf()'
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
