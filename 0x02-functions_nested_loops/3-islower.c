e43b5db0f561515792730b5f0076d0ae6b6c0354
jackson <jasonmwangi2000@gmail.com>
2022-09-13 08:10:25 -0700

    check islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _islower -Entry Point
+ *
+ * Description: This func prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (putchar(a) == c){
+			return 1;
+		} else 
+		{
+			return 0;
+		}
+}
