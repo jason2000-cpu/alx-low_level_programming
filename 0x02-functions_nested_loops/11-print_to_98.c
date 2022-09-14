04b758a98aa3d086758ef2ef9d7c56152a8d2ebc
jackson <jasonmwangi2000@gmail.com>
2022-09-14 07:29:07 -0700

    print to nth value

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,16 +15,16 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%d,\n", i);
+			printf("%d,", i);
 		}
-		printf("%d\n", 98);
+		printf("%d", 98);
 	} else if (n > 98)
 	{
 		for (i = n; i > 98; i--)
 		{
-			printf("%d,\n", i);
+			printf("%d,", i);
 		}
-		printf("%d\n", 98);
+		printf("%d", 98);
 	} else
 	{
 		printf("%d\n", n);
