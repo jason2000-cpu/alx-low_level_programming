9d596c5a4a7b1fe5103d2023c1e199dcdc187f32
jackson <jasonmwangi2000@gmail.com>
2022-09-14 03:54:34 -0700

    check is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha -Entry Point
+ *
+ * Description: This func checks if param is alphabet
+ *
+ * @c: parameter to check
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
