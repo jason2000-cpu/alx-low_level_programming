cf56b8af037628567743c84149c6d48f0de5ad8a
jackson <jasonmwangi2000@gmail.com>
2022-09-14 04:16:55 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,15 +15,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	} else if (n < 0 )
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	} else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 }
