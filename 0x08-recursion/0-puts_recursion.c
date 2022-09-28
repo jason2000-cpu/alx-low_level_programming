2a37ab64113e711a9a1777657d5f53bbe1affa0b
jackson <jasonmwangi2000@gmail.com>
2022-09-28 05:56:27 -0700

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string
+ * @s: string to print
+ * Return: char value of s
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%d\n", *s);
+	return (0);
+}
