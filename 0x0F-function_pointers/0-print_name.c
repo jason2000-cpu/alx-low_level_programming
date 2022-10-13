bb1b8439a745618bec9b34acdecabc91742f908e
jackson <jasonmwangi2000@gmail.com>
2022-10-13 01:22:04 -0700

    prints string name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*
+ * print_name - Entry Level
+ *
+ * Description: this function prints a name
+ * 
+ * Return: returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%c\n", *name);
+}
