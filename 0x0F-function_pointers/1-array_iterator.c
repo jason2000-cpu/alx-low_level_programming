5b57cd8e3c6d0261095817b65a81f373cb41a7e3
jackson <jasonmwangi2000@gmail.com>
2022-10-13 15:56:17 -0700

    a function that executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_headers.h"
+ /*
+  * arrat_iterator - Entry Level
+  * Description: This function executes a function to all elemens of an array
+  * @array: The  array to use
+  * @size: size of the array
+  * @action: a function with action to perform to all elements of the array
+  * Return : returns nothing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	int arr_ptr = &array
+	for (i = 0; i <= size; i++)
+	{
+		(*action)(arr_ptr[i]);
+	}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 #include <stdio.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
