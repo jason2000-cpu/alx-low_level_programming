ff23d493f9aedc506b0fdef829eff39cf018071e
jason2000-cpu <jacksonmuturi2000@gmail.com>
2022-10-14 02:20:39 +0300

    fixed function pointers

--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 This project mainly focus on function pointers.
-=======
-Getting started with C-Function pointers
->>>>>>> 69a816e216c12295fc9259400cf38a56dc81a2b3
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,8 +3,5 @@
 #include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-<<<<<<< HEAD
 int int_index(int *array, int size, int (*cmp)(int));
-=======
->>>>>>> 69a816e216c12295fc9259400cf38a56dc81a2b3
 #endif
