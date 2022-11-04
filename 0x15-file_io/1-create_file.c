d390139c40de147261c596b4dec6b5e6840b22d3
Jackson Muturi <62056792+jason2000-cpu@users.noreply.github.com>
2022-11-05 01:32:37 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
  * create_file - creates a file and adds permission to it
  * @filename: The name of the file to be created
  * @text_content: content to be placed in the created file
@@ -27,32 +26,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	close(fild);
-=======
- * create_file - Creates a textfile
- * @filename: The name of the file to create
- * @text_content: The text to write to created file
- * Return: Returns either 1 (succcess) or -1 (fail)
- */
-int create_file(const char *filename, char *text_content)
-{
-	int i = 0, fd;
-
-
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	while (text_content[i] != '\n')
-	{
-		i++;
-	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	fd = write(fd, text_content, i);
-	if (fd == -1)
-		return (-1);
-	close(fd);
->>>>>>> db8e878c3697afb8220fbefb977123c30ed43107
 	return (1);
 }
