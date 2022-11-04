f2b8f7a09f4b579aa0fde0644d7a33c13dc17e78
jackson <jasonmwangi2000@gmail.com>
2022-11-04 03:52:28 -0700

    create and read file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, i);
+	fd = write(fd, text_content, i);
+	if (fd == -1)
+		return (-1);
+	close(fd);
 	return (1);
 }
