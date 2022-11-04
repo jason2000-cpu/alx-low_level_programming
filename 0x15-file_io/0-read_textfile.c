f8688e48f43ead51d020649f40e4f3291c3cc565
jackson <jasonmwangi2000@gmail.com>
2022-11-04 01:53:46 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,6 @@
  * @filename: a pointer to the filename
  * @letters: The number of letters the function should read
  * Return: the number of letters it could read and print 0 otherwise
- *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -19,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd  =  open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		return(0);
+		return (0);
 	}
 	w = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, w);
