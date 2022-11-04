cc524cf6da8fe6104c6ba57deae00a7f0d6080f6
jackson <jasonmwangi2000@gmail.com>
2022-11-04 03:44:43 -0700

    create and write file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,26 +7,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int i = 0, fd;
 
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 	if (text_content == NULL)
-	{
 		text_content = "";
-	}
-	fd = open(filename, O_CREAT | O_WRONLY  | O_TRUNC, 0600);
-	if (fd == -1)
-		return(-1);
 	while (text_content[i] != '\n')
 	{
 		i++;
 	}
+	fd = open(filename, O_CREAT | O_WRONLY | o_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	write(fd, text_content, i);
-
 	return (1);
 }
-
