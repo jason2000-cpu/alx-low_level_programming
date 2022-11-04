4ef0cd6b3f38325f99bfe3beefe58d0d0a9c170d
jackson <jasonmwangi2000@gmail.com>
2022-11-04 12:34:32 -0700

    append text fo file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,8 +12,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 	while (text_content[i] != '\n')
 	{
 		i++;
@@ -21,9 +19,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename,  O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	fd = write(fd, text_content, i);
-	if (fd == -1)
-		return (-1);
+	if (test_content != NULL)
+	{
+		fd - write(fd, text_content, i);
+		if (fd == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
