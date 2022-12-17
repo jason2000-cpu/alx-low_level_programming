7dab17cb07eb4a850371118b2082eb11eb6b0903
jason2000-cpu <jasonmwangi2000@gmail.com>
2022-12-18 00:30:17 +0300

    initial commit

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,4 +1,5 @@
-#ifndef __MAIN__
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -20,6 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 
 #endif // !__MAIN__
