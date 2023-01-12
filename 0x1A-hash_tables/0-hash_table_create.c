58e30e49d05a5bee9715e054883208f0a1aec3d3
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-01-12 22:58:39 +0300

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,50 +1,27 @@
 #include "hash_tables.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
 
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-typedef struct hash_item hash_item;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-//defining hash table item
-struct hash_item {
-    char* key;
-    char* value;
-};
-
-typedef struct hash_table_t hash_table_t;
-
-//defining the Hash Table
-struct hash_table_t {
-   hash_item** items;
-   int size;
-   int count;
-};
-
-
-
-hash_item* create_item(char* key, char* value) {
-    hash_item* item = (hash_item*) malloc (sizeof(hash_item));
-    item->key = (char*) malloc (strlen(key) + 1);
-    item->value = (char*) malloc (strlen(value) + 1);
-
-    strcpy(item->key, key);
-    strcpy(item->value, value);
-    
-    return item;
-};
-
-
-//create Hash Table
-hash_table_t* create_table(unsigned long int size) {
-    hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
-    table->size = size;
-    table->count = 0;
-   
-    for (int i = 0; i<table->size; i++){
-        table->items[i] = NULL;
-    };
-
-    return table;
-};
+	return (ht);
+}
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,18 +1,16 @@
-#include "1-djb2.c"
+#include "hash_tables.h"
+
 /**
-* 
-*
-*/
+ * key_index - Get the index at which a key/value
+ *      pair should be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ * Description: Uses the djb2 algorithm.
+ */
 
-
-unsigned long int key_index(const unsigned char* key, unsigned long int size) {
-    /**
-    * key_index -  compute the index of a key
-    * @key: key to generate its index
-    * @size: size of index
-    *
-    * Return: index of key
-    */
-    unsigned long int index =  hash_djb2(key);
-    return index;
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
 }
